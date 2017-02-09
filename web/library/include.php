be5a90edd27f4e5b555007724d8bfaa390792683
Lee Crosby <ligand@hotmail.co.uk>
2017-02-09 00:06:55 +0000

    Delete include.php

--- web/library/include.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require_once(realpath(dirname(__FILE__) . "/../config/config.php"));
-	
-	require_once(realpath(dirname(__FILE__) . "/Utils.php"));
-	require_once(realpath(dirname(__FILE__) . "/BlugDbClient.php"));
-?>
